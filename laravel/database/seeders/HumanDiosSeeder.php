75e90eb4fd0a9559fccfa9aa3d68345c71c9a14b
InesRM <107609883+InesRM@users.noreply.github.com>
2022-11-20 12:11:09 +0100

    Eliminamos modelo, createDB, Dios, está incluido en USERS y no es necesario

--- laravel/database/seeders/HumanDiosSeeder.php
+++ laravel/database/seeders/HumanDiosSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class HumanoSeeder extends Seeder
+class HumanoDiosSeeder extends Seeder
 {
     /**
      * Run the database seeds.
